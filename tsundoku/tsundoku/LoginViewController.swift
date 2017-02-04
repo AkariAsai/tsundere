73f9fc50c52622994b593b6932c3f6823206dad8
Hiroaki KARASAWA <siqvare@gmail.com>
2017-02-04 22:12:58 +0900

    implement login process

--- tsundoku/tsundoku/LoginViewController.swift
+++ tsundoku/tsundoku/LoginViewController.swift
@@ -7,3 +7,112 @@
 //
 
 import Foundation
+import UIKit
+import Alamofire
+import SwiftyJSON
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+    let loginButton = FBSDKLoginButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if FBSDKAccessToken.current() != nil && false {
+            self.transitToGraphView()
+        } else {
+            initLoginButton()
+        }
+    }
+    
+    func initLoginButton() {
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+        loginButton.readPermissions = [ "public_profile", "email", "user_friends" ]
+        
+        self.view.addSubview(loginButton)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if ((error) != nil) {
+            self.showSimpleMessageDialog(title: "Authentication Error", message: "Some errors occured while executing authentication process.")
+        } else if result.isCancelled {
+            self.showSimpleMessageDialog(title: "Authentication Error", message: "Authentication denied.")
+        } else if !([ "public_profile", "email", "user_friends" ] as Set).isSubset(of: result.grantedPermissions) {
+            self.showSimpleMessageDialog(title: "Authentication Error", message: "Some permissions are not granted.")
+        } else {
+            print("Facebook Login is succeded")
+            
+            self.fetchUserData() { id, name in
+                self.registerUserInfo(id, name: name)
+                
+                self.fetchFriendsData() { friends in
+                    pushUserData(id: id, name: name, friends: friends) {
+                        self.transitToGraphView()
+                    }
+                }
+            }
+        }
+    }
+    
+    func registerUserInfo(_ id : String, name : String) {
+        let ud = UserDefaults.standard
+        
+        ud.set(id, forKey: "id")
+        ud.set(name, forKey: "name")
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    func showSimpleMessageDialog(title : String, message : String) {
+        let alert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func fetchUserData(callback : @escaping (String, String) -> Void) {
+        let parameters = [ "fields": "id,name" ]
+        
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start(completionHandler: { (connection, result, error) -> Void in
+            if error != nil {
+                print("Error: \(error)")
+            } else {
+                let object = JSON(result!)
+                let name = object["name"].string!, id = object["id"].string!
+                
+                callback(id, name)
+            }
+        })
+    }
+    
+    func fetchFriendsData(callback: @escaping ([ [ String : String ] ]) -> Void) {
+        FBSDKGraphRequest(graphPath: "me/friends", parameters: [ "fields": "id,name" ]).start(completionHandler: { (connection, result, error) -> Void in
+            if error != nil {
+                print("Error: \(error)")
+                return
+            }
+            
+            var friends : [ [String : String] ] = []
+                
+            for friend in JSON(result!)["data"] {
+                friends.append([ "id" : friend.1["id"].string!, "name" : friend.1["name"].string! ])
+            }
+            
+            callback(friends)
+        })
+    }
+    
+    func transitToGraphView() {
+        self.present(ViewController(), animated: true, completion: nil)
+    }
+}
+
--- tsundoku/tsundoku/ServerCommunication.swift
+++ tsundoku/tsundoku/ServerCommunication.swift
@@ -7,26 +7,65 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+let HOST_ADDRESS = "153.120.170.146"
 
 // - 引数: user id
 // - 返り値: 自分と友人の(username,読了ページ数,積読ページ数)の一覧
-typealias UsersInfoOverview = [ Int : (String, Int, Int) ]
+typealias UsersInfoOverview = [ String : (String, Int, Int) ]
 
-func fetchFirstView(callback : (UsersInfoOverview) -> Void) {
+func fetchFirstView(callback : @escaping (UsersInfoOverview) -> Void) {
     let overview = [
-        123 : (username: "Hiroaki KARASAWA", readNum: 1000, unreadNum: 2000),
-        124 : (username: "Kotatsu Shiraki", readNum: 2000, unreadNum: 2000),
-        125 : (username: "Akari Asai", readNum: 3000, unreadNum: 1000),
-        126 : (username: "Toby Chang", readNum: 1000, unreadNum: 5000),
-        127 : (username: "HOGE FUGA", readNum: 1000, unreadNum: 2000),
-        128 : (username: "PIYO PIYO", readNum: 2000, unreadNum: 2000),
-        129 : (username: "AIUEO KKKKK", readNum: 3000, unreadNum: 1000),
-        130 : (username: "MacBook Pro", readNum: 1000, unreadNum: 5000),
-        131 : (username: "MacBook Air", readNum: 1000, unreadNum: 2000),
-        132 : (username: "Mac Pro", readNum: 2000, unreadNum: 2000),
-        133 : (username: "MacBook", readNum: 3000, unreadNum: 1000),
-        134 : (username: "iMac", readNum: 1000, unreadNum: 5000)
-    ];
+        "123" : (username: "Hiroaki KARASAWA", readNum: 1000, unreadNum: 2000),
+        "124" : (username: "Kotatsu Shiraki", readNum: 2000, unreadNum: 2000),
+        "125" : (username: "Akari Asai", readNum: 3000, unreadNum: 1000),
+        "126" : (username: "Toby Chang", readNum: 1000, unreadNum: 5000),
+        "127" : (username: "HOGE FUGA", readNum: 1000, unreadNum: 2000),
+        "128" : (username: "PIYO PIYO", readNum: 2000, unreadNum: 2000),
+        "129" : (username: "AIUEO KKKKK", readNum: 3000, unreadNum: 1000),
+        "130" : (username: "MacBook Pro", readNum: 1000, unreadNum: 5000),
+        "131" : (username: "MacBook Air", readNum: 1000, unreadNum: 2000),
+        "132" : (username: "Mac Pro", readNum: 2000, unreadNum: 2000),
+        "133" : (username: "MacBook", readNum: 3000, unreadNum: 1000),
+        "134" : (username: "iMac", readNum: 1000, unreadNum: 5000)
+    ]
     
     callback(overview)
+    
+    return // DEBUG
+    
+    let id = UserDefaults.standard.string(forKey: "id")
+    
+    Alamofire.request("https://\(HOST_ADDRESS)/\(id)/page_number", method: .post, parameters: nil, encoding: JSONEncoding.default).responseJSON { response in
+        print("fetchFirstView: Status Code: \(response.result.isSuccess)")
+        
+        guard let object = response.result.value else { return }
+        let json = JSON(object)
+        
+        var overview : [ String: (String, Int, Int) ] = [ : ]
+        let user_id = json["user"]["user_id"].string!
+        let user_name = json["user"]["user_name"].string!
+        let read_pages = json["user"]["page_number"]["read_pages"].intValue
+        let unread_pages = json["user"]["page_number"]["unread_pages"].intValue
+        
+        overview[user_id] = (user_name, read_pages, unread_pages)
+        
+        json["friends"].arrayValue.forEach {
+            overview[$0["user_id"].string!] = ($0["user_name"].string!, $0["page_number"]["read_pages"].intValue, $0["page_number"]["unread_pages"].intValue)
+        }
+        
+        callback(overview)
+    }
 }
+
+func pushUserData(id : String, name : String, friends : [[ String : String ]], callback : (Void) -> Void) {
+    let data : [String : Any] = [ "id" : id, "name" : name, "friends" : friends ]
+    
+    Alamofire.request("https://\(HOST_ADDRESS)/user/login", method: .post, parameters: data, encoding: JSONEncoding.default).responseJSON { response in
+        print("pushUserData: Status Code: \(response.result.isSuccess)")
+    }
+}
+
+
