13aec65719f570d05b851e6167a66c17c622fd34
totetotetotem <kyarioka7945@gmail.com>
2017-02-05 06:39:57 +0900

    implement fetchBookList func

--- tsundoku/tsundoku/ServerCommunication.swift
+++ tsundoku/tsundoku/ServerCommunication.swift
@@ -60,6 +60,28 @@ func fetchFirstView(callback : @escaping (UsersInfoOverview) -> Void) {
     }
 }
 
+func fetchBookList(callback: @escaping ([ Book ]) -> Void) {
+    let id = UserDefaults.standard.string(forKey: "id")
+    Alamofire.request("https://\(HOST_ADDRESS)/\(id)/list").responseJSON { response in
+        print("fetchBookList: Status Code: \(response.result.isSuccess)")
+        guard let object = response.result.value else { return }
+        let json = JSON(object)
+        var books : [Book] = []
+        
+        json["read_books"].arrayValue.forEach {
+            let title = $0["book"]["title"].string!
+            let author = $0["book"]["author"].string!
+            let isbn = $0["book"]["ISBN"].string!
+            let page_number = $0["book"]["page_number"].intValue
+            let image_url = $0["book"]["image_url"].string!
+            
+            books.append((title, author, isbn, image_url, page_number))
+        }
+        
+        callback(books)
+    }
+}
+
 func pushUserData(id : String, name : String, friends : [[ String : String ]], callback : (Void) -> Void) {
     let data : [String : Any] = [ "id" : id, "name" : name, "friends" : friends ]
     
