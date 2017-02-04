2751c56539ec49374f4d6c1b463f800e77bdcdc7
AkariAsai <akari.1311aquamarine4@gmail.com>
2017-02-05 03:36:00 +0900

    added search

--- tsundoku/tsundoku/SearchViewController.swift
+++ tsundoku/tsundoku/SearchViewController.swift
@@ -1,26 +1,44 @@
 import UIKit
 
-class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UITextFieldDelegate{
     
+    var suggestedBooks : [(name:String, author: String)] = []
+    
+    @IBOutlet weak var suggestTableView: UITableView!
     @IBOutlet weak var searchTextField: UITextField!
+    @IBAction func didTapSearchButton(_ sender: Any) {
+        searchBookAPI(keyword: searchTextField.text!, callback : {books in for book in books {
+            print(book)
+            self.suggestedBooks.append((book.0, book.1))
+            }
+            print("hello")
+            print(self.suggestedBooks)
+            self.suggestTableView.reloadData()
+        })
+        
+    }
     
-    let titles = ["こころ", "舞姫", "吾輩は猫である"]
+    typealias Book = (String, String, String, String, Int)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchTextField.delegate = self
+        //searchTextField.addTarget(self, action: #selector(SearchViewController.textFieldDidChange(_:)), for: UIControlEvents.editingChanged)
     }
     
     //GoogleBooksAPIから返される情報の数によって要更新
     internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titles.count
+        return suggestedBooks.count
     }
     
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let suggestionCell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "SuggestionCell", for: indexPath)
         
         // セルに表示する値を設定する
-        suggestionCell.textLabel!.text = titles[indexPath.row]
+        suggestionCell.textLabel!.text = suggestedBooks[indexPath.row].name
+        suggestionCell.detailTextLabel!.text = suggestedBooks[indexPath.row].author
         return suggestionCell
     }
     
@@ -33,6 +51,4 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    
 }
