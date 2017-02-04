495202a7893518fb586bb1896141ac7d3b328b09
Hiroaki KARASAWA <siqvare@gmail.com>
2017-02-05 08:45:29 +0900

    remove

--- tsundoku/tsundoku/IndivisualPageViewController.swift
+++ tsundoku/tsundoku/IndivisualPageViewController.swift
@@ -1,14 +1,5 @@
 import UIKit
 
-extension UIColor {
-    clasUserInterfaceState.xcuserstates func rgb(r: Int, g: Int, b: Int, alpha: CGFloat) -> UIColor{
-        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: alpha)
-    }
-    class func MainColor() -> UIColor {
-        return UIColor.rgb(r: 0, g: 188, b: 188, alpha: 1.0)
-    }
-}
-
 
 class IndividualPageViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
     
